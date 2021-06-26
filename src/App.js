f95a0696c9f7f42b8a4f4984d89eb756c6ac4f0d
Will Connell <willconnell@utexas.edu>
2021-06-26 11:24:26 -0500

    Fixed dynamic formatting of cards

--- src/App.js
+++ src/App.js
@@ -7,29 +7,33 @@ function App() {
   return (
     <div className="App">
       <Sidebar />
-      <div className="row main-container offset-3">
+      <div className="offset-3">
         <h1>PlayerList</h1>
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <p>
-          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-          culpa qui officia deserunt mollit anim id est laborum."
-        </p>
+      </div>
+      <div className="container">
+        <div className="row offset-3 d-flex justify-content-around">
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <p>
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum."
+          </p>
+        </div>
       </div>
     </div>
   );
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -5,22 +5,22 @@ const Sidebar = () => {
     <div className="leftnav bg-light col-3">
       <h3>Navigation</h3>
       <p>
-        <button className="btn btn-primary">Add</button>
+        <button className="btn btn-primary">Add Song</button>
       </p>
 
       <ul class="list-group">
-        <li class="list-group-item d-flex justify-content-between align-items-center">
+        <button class="list-group-item d-flex justify-content-between align-items-center">
           A list item
           <span class="badge bg-primary rounded-pill">14</span>
-        </li>
-        <li class="list-group-item d-flex justify-content-between align-items-center">
+        </button>
+        <button class="list-group-item d-flex justify-content-between align-items-center">
           A second list item
           <span class="badge bg-primary rounded-pill">2</span>
-        </li>
-        <li class="list-group-item d-flex justify-content-between align-items-center">
+        </button>
+        <button class="list-group-item d-flex justify-content-between align-items-center">
           A third list item
           <span class="badge bg-primary rounded-pill">1</span>
-        </li>
+        </button>
       </ul>
 
       <div class="list-group" id="list-tab" role="tablist">
